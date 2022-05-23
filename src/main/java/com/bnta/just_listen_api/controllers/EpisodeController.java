eba864598dd3cbee40f182a06748c436b39567a5
Patrick Merchant <patrickmerchant9@gmail.com>
2022-05-23 19:53:01 +0100

    post route added in ep controller

--- src/main/java/com/bnta/just_listen_api/controllers/EpisodeController.java
+++ src/main/java/com/bnta/just_listen_api/controllers/EpisodeController.java
@@ -5,10 +5,7 @@ import com.bnta.just_listen_api.repositories.EpisodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +28,13 @@ public class EpisodeController {
     public ResponseEntity<Optional<Episode>> getEpisode(@PathVariable Long id) {
         var episode = episodeRepository.findById(id);
         return new ResponseEntity<>(episode, episode.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+    }
+
+    // POST
+    @PostMapping
+    public ResponseEntity createEpisode(@RequestBody Episode newEpisode) {
+        episodeRepository.save(newEpisode);
+        return new ResponseEntity<>(newEpisode, HttpStatus.CREATED);
     }
 
 
