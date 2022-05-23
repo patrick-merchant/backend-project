0d04d0cbaed6a68e7d99dcd0ddd091c81b44bb43
Sabina Rafique <sabina.r@hotmail.co.uk>
2022-05-23 17:17:47 +0100

    Maven project created

---
+++ src/main/java/com/bnta/just_listen_api/JustListenApiApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.just_listen_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustListenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustListenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnta/just_listen_api/JustListenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.just_listen_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustListenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
