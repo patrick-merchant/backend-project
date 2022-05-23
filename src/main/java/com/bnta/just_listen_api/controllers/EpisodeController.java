712ecb5633d93a7d6023540d57b730cd6f16105e
Patrick Merchant <patrickmerchant9@gmail.com>
2022-05-23 19:46:54 +0100

    index route added in ep controller

--- src/main/java/com/bnta/just_listen_api/controllers/EpisodeController.java
+++ src/main/java/com/bnta/just_listen_api/controllers/EpisodeController.java
@@ -1,4 +1,29 @@
 package com.bnta.just_listen_api.controllers;
 
+import com.bnta.just_listen_api.models.Episode;
+import com.bnta.just_listen_api.repositories.EpisodeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("episodes")
 public class EpisodeController {
+
+    @Autowired
+    private EpisodeRepository episodeRepository;
+
+    // INDEX
+    @GetMapping
+    public ResponseEntity<List<Episode>> getEpisodes() {
+        return new ResponseEntity<>(episodeRepository.findAll(), HttpStatus.OK);
+    }
+
+    
 }
+
