ac0e32f13c86a6aa40af656fddbc0bf95308159c
Sabina Rafique <sabina.r@hotmail.co.uk>
2022-05-23 19:51:37 +0100

    Show route added to Podcast Controller

--- src/main/java/com/bnta/just_listen_api/controllers/PodcastController.java
+++ src/main/java/com/bnta/just_listen_api/controllers/PodcastController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("podcasts")
@@ -23,4 +25,12 @@ public class PodcastController {
     public ResponseEntity<List<Podcast>> getPodcasts() {
         return new ResponseEntity<>(podcastRepository.findAll(), HttpStatus.OK);
     }
+
+    // SHOW
+    @GetMapping("/{id}") // localhost:8080/podcasts/1 (or any other id number i.e. 2, 3 etc.)
+    public ResponseEntity<Optional<Podcast>> getPodcast(@PathVariable Long id) {
+        var product = podcastRepository.findById(id);
+        return new ResponseEntity<>(product, product.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+    }
+
 }
