d434139b745b22066d176d15ab7bae11b44b41c1
Sabina Rafique <sabina.r@hotmail.co.uk>
2022-05-24 13:41:55 +0100

    Derived queries added to episode repository

--- src/main/java/com/bnta/just_listen_api/repositories/EpisodeRepository.java
+++ src/main/java/com/bnta/just_listen_api/repositories/EpisodeRepository.java
@@ -3,5 +3,15 @@ package com.bnta.just_listen_api.repositories;
 import com.bnta.just_listen_api.models.Episode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
+
+    // DERIVED QUERIES
+    List<Episode> findEpisodeByNameContainingIgnoreCase (String name);
+    List<Episode> findEpisodeByDescriptionContainingIgnoreCase (String description);
+   // List<Episode> findEpisodeByMostRecentlyPostedDesc (LocalDate datePosted);
+    List<Episode> findByEpisodeDurationLessThan (int duration);
+
 }
