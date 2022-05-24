3887b7df9ea7da164ad71d0aa8a12402a1e79ec0
AnaPaulaL27 <apl.moreno@hotmail.com>
2022-05-24 13:02:46 +0100

    removed arraylist import statement in episode

--- src/main/java/com/bnta/just_listen_api/models/Episode.java
+++ src/main/java/com/bnta/just_listen_api/models/Episode.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
