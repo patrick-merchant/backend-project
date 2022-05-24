a8555132d0f97bd9aa6733d902de81b389b86cdf
Sabina Rafique <sabina.r@hotmail.co.uk>
2022-05-24 14:29:00 +0100

    Derived queries added to contributor repository

--- src/main/java/com/bnta/just_listen_api/repositories/EpisodeRepository.java
+++ src/main/java/com/bnta/just_listen_api/repositories/EpisodeRepository.java
@@ -3,7 +3,6 @@ package com.bnta.just_listen_api.repositories;
 import com.bnta.just_listen_api.models.Episode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public interface EpisodeRepository extends JpaRepository<Episode, Long> {
