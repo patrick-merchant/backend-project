b84948b5d7ba022817ee88e0a9689d6372f6442c
AnaPaulaL27 <apl.moreno@hotmail.com>
2022-05-24 13:43:22 +0100

    Added update method into episode controller

--- src/main/java/com/bnta/just_listen_api/controllers/EpisodeController.java
+++ src/main/java/com/bnta/just_listen_api/controllers/EpisodeController.java
@@ -1,5 +1,6 @@
 package com.bnta.just_listen_api.controllers;
 
+import com.bnta.just_listen_api.models.Contributor;
 import com.bnta.just_listen_api.models.Episode;
 import com.bnta.just_listen_api.repositories.EpisodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,21 @@ public class EpisodeController {
     public ResponseEntity<Optional<Episode>> getEpisode(@PathVariable Long id) {
         var episode = episodeRepository.findById(id);
         return new ResponseEntity<>(episode, episode.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+    }
+    //UPDATE
+
+    @PutMapping(value="/episodes/{id}") // localhost:8080/episodes/1 (or any other id number instead of 1)
+    public ResponseEntity<Episode> putEpisode(@RequestBody Episode episode, @PathVariable Long id){
+        Episode episodeToUpdate = episodeRepository.findById(id).get();
+        episodeToUpdate.setName(episode.getName());
+        episodeToUpdate.setDescription(episode.getDescription());
+        episodeToUpdate.setDuration(episode.getDuration());
+        episodeToUpdate.setDatePosted(episode.getDatePosted());
+        episodeToUpdate.setContributors(episode.getContributors());
+        episodeToUpdate.setPodcast(episode.getPodcast());
+
+        episodeRepository.save(episodeToUpdate);
+        return new ResponseEntity<>(episodeToUpdate, HttpStatus.OK);
     }
 
     // POST
